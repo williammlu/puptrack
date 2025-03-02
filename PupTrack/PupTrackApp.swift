badae2005726d25a75fd1a2302d25a6eef8d776a
William Lu <lu.william.m@gmail.com>
2025-03-01 21:05:10 -0800

    Initial Commit

---
+++ PupTrack/PupTrackApp.swift
@@ -0,0 +1,17 @@
+//
+//  PupTrackApp.swift
+//  PupTrack
+//
+//  Created by William Lu on 3/1/25.
+//
+
+import SwiftUI
+
+@main
+struct PupTrackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PupTrackTests/PupTrackTests.swift
@@ -0,0 +1,17 @@
+//
+//  PupTrackTests.swift
+//  PupTrackTests
+//
+//  Created by William Lu on 3/1/25.
+//
+
+import Testing
+@testable import PupTrack
+
+struct PupTrackTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
